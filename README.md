77ba52feb533bac24bf59686f69132dc0ba2abe8
LukasWestholt <support@lukaswestholt.de>
2022-01-27 14:06:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# studNet Leipzig shell script
+
+(Forked from https://github.com/kinafu)
+
+Hello.
+
+Install the client on a machine running Linux. A low-power client like the Raspberry Pi is recommended.
+If you want to stay signed on in StudNET 24/7, the client must be online (=powered on) 24/7, too.
+
+After the installation, StudNET client should autostart upon each boot.
+The client runs quietly as a service in the background.
+
+## Installation
+
+    sudo apt update && sudo apt upgrade
+    sudo apt install openssh-client sshpass ca-certificates curl unattended-upgrades $additional_packages -y
+    nano studnet.sh
+    <edit studnet_nr and studnet_nr and save changes by ctrl-s then ctrl-x >
+    sudo cp studnet.sh /usr/local/bin/studnet.sh
+    sudo chmod u+rwx /usr/local/bin/studnet.sh
+    sudo cp studnet.service /etc/systemd/system/studnet.service
+
+### Autostart
+    systemctl daemon-reload
+    systemctl enable studnet.service
+### Run manually
+    service studnet start
+
+### on raspberry
+you can enable watchdog see [here](https://github.com/kinafu/studNET-client/blob/master/installer.sh).
+
+### Credits:
+https://github.com/kinafu/studNET-client
+
+https://unix.stackexchange.com/a/654079
+
+https://askubuntu.com/q/936728
+
---
+++ studnet.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+studnet_nr=""
+studnet_password=""
+studnet_server_ip="139.18.143.253"
+host=https://google.de
+
+while true; do
+  #now=$(date +"%T")
+  #echo "Current time : $now"
+  curl --head --silent --connect-timeout 2 "$host" >/dev/null
+  error_code=$?
+
+  if [ "$error_code" -gt 0 ]; then
+    echo "Pinging $host was unsucessful." 1>&2
+    echo "Reconnecting now"
+    # kill 1st background job in current session, which is our (old) ssh session
+    # kill %1 > /dev/null 2>&1
+    # sleep 3
+    # sshpass -p "$studnet_password" ssh -tt -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=2  "$studnet_nr"@"$studnet_server_ip" &
+    # sshpass -p "$studnet_password" autossh -vv -M 0 -tt -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -o ServerAliveCountMax=2  "$studnet_nr"@"$studnet_server_ip"
+    sshpass \
+      -v \
+      -p "$studnet_password" \
+      ssh \
+      -tt \
+      -o "ServerAliveInterval 15" \
+      -o "ServerAliveCountMax 2" \
+      -o "ConnectTimeout 10" \
+      -o "ExitOnForwardFailure yes" \
+      -o "StrictHostKeyChecking=yes" \
+      -4 "$studnet_nr"@"$studnet_server_ip"
+    echo "SSH connection exited, wait 10s before re-trying"
+    sleep 10
+  else
+    sleep 120
+  fi
+done
